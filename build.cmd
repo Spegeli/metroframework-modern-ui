1dfa1669aab27a071196d13a4f5fda5dc340b2da
Spegeli <andreas.spegel@me.com>
2017-04-13 20:34:37 +0200

    Build.cmd Update

--- build.cmd
+++ build.cmd
@@ -2,8 +2,7 @@
 
 set config=Release
 set inputdir=%~dp0
-set outputdir=C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\build
-set cwd=%CD%
+set outputdir=C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\compiled
 set commonflags=/p:Configuration=%config%;AllowUnsafeBlocks=true /p:CLSCompliant=False
 
 if %PROCESSOR_ARCHITECTURE%==x86 (
@@ -14,27 +13,26 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 :build
 echo ---------------------------------------------------------------------
 echo Building AnyCpu release...
-%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v2.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET20"
+%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v2.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\NET20"
 if errorlevel 1 goto build-error
-%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET35"
+%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\NET35"
 if errorlevel 1 goto build-error
-%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET40"
+%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\NET40"
 if errorlevel 1 goto build-error
-%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET45"
+%msbuild% "%inputdir%\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\NET45"
 if errorlevel 1 goto build-error
 
+timeout 10 > NUL
 
-
-
-MetroFramework\MetroFramework.csproj
-
-:build-error
-echo Failed to compile...
-pause
+For /R %outputdir%\ %%G IN (*.pdb *.exe *.config) do DEL /F "%%G"
+FOR /D /R %outputdir%\ %%G IN (Data) DO @IF EXIST "%%G" rd /s /q "%%G"
 
 :done
 echo.
 echo ---------------------------------------------------------------------
 echo Compile finished....
-cd %cwd%
+pause
+
+:build-error
+echo Failed to compile...
 pause
