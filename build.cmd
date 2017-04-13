585d776c6f9d2b632c4a7d0aab6c5f15cdf3a868
Spegeli <andreas.spegel@me.com>
2017-04-13 18:08:24 +0200

    Update Build.cmd

--- build.cmd
+++ build.cmd
@@ -13,6 +13,10 @@ if %PROCESSOR_ARCHITECTURE%==x86 (
 :build
 echo ---------------------------------------------------------------------
 echo Building AnyCpu release...
+%msbuild% "C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\metroframework-modern-ui-master\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v2.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET20"
+if errorlevel 1 goto build-error
+%msbuild% "C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\metroframework-modern-ui-master\MetroFramework.sln" %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET35"
+if errorlevel 1 goto build-error
 %msbuild% "C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\metroframework-modern-ui-master\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET40"
 if errorlevel 1 goto build-error
 %msbuild% "C:\Users\Andreas\Dropbox\VisualStudio\DLLs\Themes\MetroFramework\metroframework-modern-ui-master\MetroFramework.sln" %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\AnyCpu\NET45"
